f75ec585a6c8878720df1b477aae7bdec8c67df2
AKSHATkumar22 <118597336+AKSHATkumar22@users.noreply.github.com>
2024-07-21 21:30:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slide");
+
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  showSlides(slideIndex);
+});
